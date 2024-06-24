b7c5b19668360db52cb6f21b3fe6baa0f298f604
Msebeyenjabulo <Mag24012@byui.edu>
2024-06-24 08:07:17 +0200

    web page

---
+++ scripts/gatedates.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const yearSpan = document.getElementById('currentyear');
+    const lastModified = document.getElementById('lastModified');
+
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+});
