34af3b543ee0855d8567689b81459bd85bcd0622
Msebeyenjabulo <Mag24012@byui.edu>
2024-07-14 22:00:02 +0200

    FORM

--- scripts/form.js
+++ scripts/form.js
@@ -1,32 +1,37 @@
 const products = [
     {
         id: 'fc-1888',
-        name: 'flux capacitor',
-        averageRating: 4.5
+        name: "Flux Capacitor",
+        avgRating: 4.5
     },
     {
         id: 'fc-2050',
-        name: 'power laces',
-        averageRating: 4.7
+        name: "Power Laces",
+        avgRating: 4.7
     },
     {
         id: 'fs-1987',
-        name: 'time circuits',
-        averageRating: 3.5
+        name: "Time Circuits",
+        avgRating: 3.5
     },
     {
         id: 'ac-2000',
-        name: 'low voltage reactor',
-        averageRating: 3.9
+        name: "Low Voltage Reactor",
+        avgRating: 3.9
     },
     {
         id: 'jj-1969',
-        name: 'warp equalizer',
-        averageRating: 5.0
+        name: "Warp Equalizer",
+        avgRating: 5.0
     }
 ];
 
-function populateProductOptions() {
+document.addEventListener('DOMContentLoaded', function() {
+    populateProductNameOptions();
+    populateFeatureCheckboxes();
+});
+
+function populateProductNameOptions() {
     const productNameSelect = document.getElementById('productName');
 
     products.forEach(product => {
@@ -44,7 +49,7 @@ function populateFeatureCheckboxes() {
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.id = `feature-${product.id}`;
-        checkbox.name = 'usefulFeatures';
+        checkbox.name = 'usefulFeatures'; 
         checkbox.value = product.name;
 
         const label = document.createElement('label');
@@ -56,17 +61,3 @@ function populateFeatureCheckboxes() {
         featureCheckboxes.appendChild(document.createElement('br'));
     });
 }
-
-// event listener when the document is loaded
-document.addEventListener('DOMContentLoaded', function() {
-    populateProductOptions();
-    populateFeatureCheckboxes();
-
-    // current year
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentYear').textContent = currentYear;
-
-    // last modified date
-    const lastModifiedDate = document.lastModified;
-    document.getElementById('lastModified').textContent = lastModifiedDate;
-});
